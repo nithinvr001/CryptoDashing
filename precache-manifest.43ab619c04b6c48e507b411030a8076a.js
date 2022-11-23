3619f56d6a1a2c641575b247fc7d556303c03d31
nithinvr001 <nithinr.18.beis@acharya.ac.in>
2022-11-23 13:47:27 +0530

    Updates

---
+++ precache-manifest.43ab619c04b6c48e507b411030a8076a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "ac035d460f2625d937c1",
+    "url": "/CryptoDashing/static/css/main.04ad51b9.chunk.css"
+  },
+  {
+    "revision": "ac035d460f2625d937c1",
+    "url": "/CryptoDashing/static/js/main.ac035d46.chunk.js"
+  },
+  {
+    "revision": "fbee9149cc341e4ff9a0",
+    "url": "/CryptoDashing/static/js/1.fbee9149.chunk.js"
+  },
+  {
+    "revision": "bf40f12645e237ec1a78",
+    "url": "/CryptoDashing/static/js/2.bf40f126.chunk.js"
+  },
+  {
+    "revision": "f0d0892b7b2c9a45fc5c",
+    "url": "/CryptoDashing/static/js/runtime~main.f0d0892b.js"
+  },
+  {
+    "revision": "628a822a5599e64a3dc436b96d64f6ce",
+    "url": "/CryptoDashing/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.7bd85057bc325ed206a888ee05cce8d6.js"
+  "/CryptoDashing/precache-manifest.43ab619c04b6c48e507b411030a8076a.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/CryptoDashing/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
